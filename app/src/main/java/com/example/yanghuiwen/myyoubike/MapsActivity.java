6617e1a1e231e831e348f6cdda99f4224193844c
kikiouo <kikiouo201@gmail.com>
2019-02-10 18:38:23 +0800

    Add Location to use google map

--- app/src/main/java/com/example/yanghuiwen/myyoubike/MapsActivity.java
+++ app/src/main/java/com/example/yanghuiwen/myyoubike/MapsActivity.java
@@ -1,10 +1,17 @@
 package com.example.yanghuiwen.myyoubike;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
-import com.example.yanghuiwen.myyoubike.model.StationResponse;
-import com.example.yanghuiwen.myyoubike.model.StationResponse.Station;
 import com.example.yanghuiwen.myyoubike.util.Jsonutil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,18 +19,22 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.gson.Gson;
 
-import java.util.Map;
+import java.util.List;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
-
-
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+
 
     private GoogleMap mMap;
     Jsonutil bike_json;
+    LocationManager lm;
+    private String provider;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,46 +42,237 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        checkLocationPermission();
+
+
         if (mapFragment != null)
             mapFragment.getMapAsync(this);
-        bike_json=new Jsonutil();
+        bike_json = new Jsonutil();
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        List<String> list = lm.getProviders(true);
 
-        Log.d("test","我進去了onCreate");
+        if (list.contains(LocationManager.NETWORK_PROVIDER)) {
+            //是否为网络位置控制器
+            provider = LocationManager.NETWORK_PROVIDER;
+            Toast.makeText(this, "网络位置控制器",
+                    Toast.LENGTH_LONG).show();
+        }
+        else if (list.contains(LocationManager.GPS_PROVIDER)) {
+            //是否为GPS位置控制器
+            provider = LocationManager.GPS_PROVIDER;
+            Toast.makeText(this, "GPS位置控制器",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "請檢查網路或GPS是否打开",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        Log.d("test", "我進去了onCreate");
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage(R.string.text_location_permission)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MapsActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                        lm.requestLocationUpdates(provider, 400, 1,mLocationListener);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
+    }
+
+
+                /**
+                 * Manipulates the map once available.
+                 * This callback is triggered when the map is ready to be used.
+                 * This is where we can add markers or lines, add listeners or move the camera. In this case,
+                 * we just add a marker near Sydney, Australia.
+                 * If Google Play services is not installed on the device, the user will be prompted to install
+                 * it inside the SupportMapFragment. This method will only be triggered once the user has
+                 * installed Google Play services and returned to the app.
+                 */
+
+    private LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+                Log.d("test",String.format("%f, %f", location.getLatitude(), location.getLongitude()));
+                drawMarker(location);
+                lm.removeUpdates(mLocationListener);
+            } else {
+                Log.d("test","Location is null");
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+        }
+    };
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng sydney=null;
-        String jsonStr = bike_json.loadJSONFromAsset(getAssets(),"T_bike.json");
-        Gson gson = new Gson();
-        StationResponse station = gson.fromJson(jsonStr ,StationResponse .class);
-        Map<String, Station> stations=station.getRetVal();
-        try {
-            for (int i = 2001; i < 2010; i++) {
-                sydney = new LatLng(Double.valueOf(stations.get(i + "").getLat()), Double.valueOf(stations.get(i + "").getLng()));
-                mMap.addMarker(new MarkerOptions().position(sydney).title(stations.get(i + "").getSna()));
-            }
-        }catch (NullPointerException e){
-            e.printStackTrace();
-        }
+//        LatLng sydney = null;
+//        String jsonStr = bike_json.loadJSONFromAsset(getAssets(), "T_bike.json");
+//        Gson gson = new Gson();
+//        StationResponse station = gson.fromJson(jsonStr, StationResponse.class);
+//        Map<String, StationResponse.Station> stations = station.getRetVal();
+//        try {
+//            for (int i = 2001; i < 2010; i++) {
+//                sydney = new LatLng(Double.valueOf(stations.get(i + "").getLat()), Double.valueOf(stations.get(i + "").getLng()));
+//                mMap.addMarker(new MarkerOptions().position(sydney).title(stations.get(i + "").getSna()));
+//            }
+//        } catch (NullPointerException e) {
+//            e.printStackTrace();
+//        }
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
 //
-////        }
+//       }
+//  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+        locationPoint();
 
-       mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 
+    private void locationPoint() {
+        double lat, lng;
+        Log.i("test","媽!我在這！locationPoint() ");
+
+
+//        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    Activity#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for Activity#requestPermissions for more details.
+//
+//            return;
+//        }
+//        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER); // 設定定位資訊由 GPS提供
+
+        Location location=null;
+        try {
+            location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        } catch (SecurityException e) {
+            //dialogGPS(this.getContext()); // lets the user know there is a problem with the gps
+            Log.i("test", "SecurityException");
+        }
+
+        mLocationListener.onLocationChanged(location);
+        Log.i("test", "location="+location);
+        if(location!=null) {
+            lat = location.getLatitude();  // 取得經度
+            lng = location.getLongitude(); // 取得緯度
+
+            LatLng HOME = new LatLng(lat, lng);
+            mMap.addMarker(new MarkerOptions().position(HOME).title("現在位址"));
+            Log.i("test", "媽!我在這！");
+           // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HOME, 15.0f));//數字越大放越大
+
+        }
+
+
+
+
+    }
+
+    private void getCurrentLocation() {
+        boolean isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Location location = null;
+        if (!(isGPSEnabled || isNetworkEnabled)) {
+            //  Snackbar.make(mMapView, R.string.error_location_provider, Snackbar.LENGTH_INDEFINITE).show();
+        }else {
+
+        }
+        if (location != null)
+            drawMarker(location);
+    }
+
+    /* 在Google Map上放上目前位置的地標圖示。 */
+    private void drawMarker(Location location) {
+        if (mMap != null) {
+            mMap.clear();
+            LatLng gps = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.addMarker(new MarkerOptions()
+                    .position(gps)
+                    .title("Current Position"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(gps, 12));
+        }
+    }
 
 }
